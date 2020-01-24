dbb6cee34dd55298b19dbc6980c144085ceebd42
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2020-01-24 10:54:47 +0100

    Refactored configuration
    Moved configuration to org file and added ivy.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,3 @@
-;; EMACS CONFIGURATION
+(package-initialize)
 
-;; Load path
-(add-to-list 'load-path (expand-file-name "packages" user-emacs-directory))
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-;; Packages
-(require 'package-conf)
-(require 'evil-conf)
-
-;; Typing
-(electric-pair-mode)
-(ido-mode t)
-(winner-mode t)
-
-;; Visuals
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(show-paren-mode)
-(global-hl-line-mode t)
-(load-theme 'wombat)
+(org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
