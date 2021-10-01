3779cb344bd61dd6944ca4f89b26a8bf08f6647b
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-10-01 14:12:05 +0200

    Simplified doom emacs configuration

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -6,7 +6,3 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
-
-(package! nov)
-(package! paren)
-(package! org-drill)
