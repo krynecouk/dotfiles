8cd0c1458790c15f87ba2bb89db0fb833a15f9b1
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-10-03 16:21:32 +0200

    Removed specific root from push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 copyDoom() { cp ~/.doom.d/{config.org,init.el,packages.el} ~/dev/dotfiles/.doom.d } && \
 copyNvim() { cp ~/.config/nvim/{init.vim,plug.vim,ui.vim} ~/dev/dotfiles/nvim } && \
-copyVscode() { cp /Users/darius.kryszczuk/Library/Application\ Support/Code/User/{settings.json,keybindings.json} ~/dev/dotfiles/.vscode } && \
+copyVscode() { cp ~/Library/Application\ Support/Code/User/{settings.json,keybindings.json} ~/dev/dotfiles/.vscode } && \
 copyIdeavimrc() { cp ~/.ideavimrc ~/dev/dotfiles/.idea/.ideavimrc } && \
 copyDoom && copyNvim && copyVscode && copyIdeavimrc && git add . && git commit -a
